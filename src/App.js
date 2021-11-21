597153db69bd7f72eba02f6561247b6bf06aa323
JasonSawxd <yeisonuyaguari1@gmail.com>
2021-11-21 17:21:18 -0500

    Creation the sidebar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { Route, Routes } from 'react-router-dom';
 
 //Componentes.
 import Ordenes from './components/pages/ordenes';
-
+import Menu from './components/pages/menu';
+import NuevoPlatillo from './components/pages/nuevoPlatillo';
+import Sidebar from './components/ui/sidebar';
 function App() {
   return (
-    <div>
-      <Routes>
+    <div className="md:flex min-h-screen">
+    <Sidebar/>
+    <div className="md:w2/5 xl:w-1/5">
+    <Routes>
         <Route path="/" exact element={<Ordenes/>} />
+        <Route path="/menu" exact element={<Menu/>} />
+        <Route path="/nuevo_Platillo" exact element={<NuevoPlatillo/>}/>
       </Routes>
+    </div>
     </div>
   );
 }
---
+++ src/components/pages/menu.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Menu = () => {
+    return ( 
+        <>
+        <h1 className=" text-3xl to-black"> Menu</h1>
+        </>
+    );
+}
+export default Menu;
